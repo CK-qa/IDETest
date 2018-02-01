bb9ca50806e410bb6600f9c826befdc3da667542
victoriabozhko <victoria.bozhko@gmail.com>
2018-02-01 18:52:37 +0300

    some changes done

--- standard_test/src/main/java/ClassB.java
+++ standard_test/src/main/java/ClassB.java
@@ -1,44 +1,48 @@
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-
 public class ClassB extends ClassA implements Interface {
 
-    List<String> list;
-    Map<Integer, String> map;
-    Set<Integer> set;
+    static String one;
+    static String two;
 
-    public ClassB(List<String> list, Map<Integer, String> map, Set<Integer> set) {
-        this.list = list;
-        this.map = map;
-        this.set = set;
-    }
 
-    public void bar() {
-
+    public static void function() {
+        if (one.contains("A")) {
+            System.out.println("...yes...");
+            if (two.contains("!")) {
+                System.out.println("Bingo!");
+            } else System.out.println("You're loser");
+        }
     }
 
     private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final String ALPHA_NUMERIC_STRING_2 = "!@#$%^&*()_+";
 
-    private static String randomAlphaNumeric(int count) {
+    private static String randomAlphaNumeric(int count, String randomSource) {
         StringBuilder builder = new StringBuilder();
         for (int i = count; i > 0 ; i--) {
 
-            int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
-            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+            int character = (int) (Math.random() * randomSource.length());
+            builder.append(randomSource.charAt(character));
         }
         return builder.toString();
     }
 
     public static void main(String[] args) {
-        String one = randomAlphaNumeric(5);
-        String two = randomAlphaNumeric(5);
+        one = randomAlphaNumeric(5, ALPHA_NUMERIC_STRING);
+        two = randomAlphaNumeric(5, ALPHA_NUMERIC_STRING_2);
+
 
         if (one.equals(two)) {
+            System.out.println("one (" + one + ") == two (" + two + ")");
             System.out.println("one is " + one + " and it's equal to two which is " + two);
         } else {
+            System.out.println("one (" + one + ") != two (" + two + ")");
             System.out.println("one is " + one + " and it's not equal to two which is " + two);
         }
+
+        function();
+    }
+
+    public void function_1() {
+
     }
 }
