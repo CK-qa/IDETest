14931e30b6d70f1b777cd5050ab512da37bc00ac
viktoria.bozhko <victoria.bozhko@gmail.com>
2018-02-08 19:04:32 +0300

    some added

--- labeling/src/main/java/core_labels/Calcs.java
+++ labeling/src/main/java/core_labels/Calcs.java
@@ -1,48 +1,37 @@
 package core_labels;
 
-import java.lang.annotation.Annotation;
+import java.math.BigDecimal;
+import java.util.Scanner;
 
-@AnnotationToTest(
-        name = "Vika",
-        purpose = ""
-)
+public class Labels {
 
-public class Calcs implements FInterface, AnnotationToTest {
+    private static void oddNumbers() {
 
-    private static Calcs calcs = new Calcs();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number till which you wanna get odd numbers: ");
+        int num = scanner.nextInt();
+        int var;
+
+        for (var = 0; var <= num; var++) {
+            if (var%2 != 0) {
+                System.out.print(var + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void power() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        Integer num = scanner.nextInt();
+        System.out.println("Enter a power of the number: ");
+        Integer pow = scanner.nextInt();
+
+        System.out.println(BigDecimal.valueOf(Math.pow(num, pow)));
+    }
 
     public static void main(String[] args) {
-        MClass.oddNumbers();
-        MClass.power();
-        calcs.anyCalc();
-        calcs.doSomeOtherWork();
-        test(1,2,3, "4", true);
+        oddNumbers();
+        power();
     }
-
-    @Override
-    public void anyCalc() {
-        System.out.println(Math.log10(100));
-    }
-
-    @Override
-    public String name() {
-        return null;
-    }
-
-    @Override
-    public String purpose() {
-        return null;
-    }
-
-    @Override
-    public Class<? extends Annotation> annotationType() {
-        return null;
-    }
-
-    private static void test(int a, int b, int c, String str, boolean bb) {
-        System.out.println(a + b + c + str + bb);
-    }
-
-
-
 }
