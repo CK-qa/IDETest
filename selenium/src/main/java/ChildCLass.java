1d7878fecfc7e3ca9e9ac9bcd94d22c952e90236
victoriabozhko <victoria.bozhko@gmail.com>
2018-02-14 19:48:30 +0300

    Merge branch 'master' of /Users/victoriabozhko/IdeaProjects/IDETest with conflicts.

---
+++ selenium/src/main/java/ChildCLass.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.Map;
+
+public class ChildCLass extends ParentClass {
+
+    public static int b;
+    int a;
+    int c;
+    Map<String, Integer> map;
+    List<Object> list;
+}
