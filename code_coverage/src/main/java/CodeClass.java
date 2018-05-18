337d32feb79501387b7a6b216bb604f2e56571f9
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2018-05-18 18:45:16 +0300

    new module added

---
+++ code_coverage/src/main/java/CodeClass.java
@@ -0,0 +1,10 @@
+public class CodeClass {
+
+    public static String bar(String str) {
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(bar("pewpew"));
+    }
+}
