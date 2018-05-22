60b2910f9e6551f5d85db29d093b161781e0e66c
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2018-05-22 14:57:01 +0300

    changes done

--- labeling/src/main/java/core_labels/Calcs.java
+++ labeling/src/main/java/core_labels/Calcs.java
@@ -7,8 +7,8 @@ import static core_labels.MClass.power;
 
 @SuppressWarnings("ALL")
 @AnnotationToTest(
-        name = "Vika",
-        purpose = ""
+      name = "Vika",
+      purpose = ""
 )
 
 public class Calcs implements FInterface, AnnotationToTest {
@@ -18,24 +18,23 @@ public class Calcs implements FInterface, AnnotationToTest {
         Calcs calcs = new Calcs();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number till which you wanna get odd numbers: ");
+        System.out.print("Enter any number to get odd numbers before it:\t");
         int num = scanner.nextInt();
         int var;
 
         for (var = 0; var <= num; var++) {
-            if (var%2 != 0) {
+            if (var % 2 != 0) {
                 System.out.print(var + " ");
             }
         }
-    System.out.println();
+        System.out.println();
     }
 
     /**
-     *
+     * @param a is just int
+     * @param a is just int
      * @param a is just int
      * @return sum of two ints
-     * @param a is just int
-     * @param a is just int
      */
     private static int method(int a) {
         return a + a;
