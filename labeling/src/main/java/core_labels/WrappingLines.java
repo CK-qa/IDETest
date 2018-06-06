f1bb8a1252eb0fe861c7f5b2fcd89abe22aba5d5
vbozhko <viktoria.bozhko@jetbrains.com>
2018-06-06 17:33:28 +0300

    commit

--- labeling/src/main/java/core_labels/WrappingLines.java
+++ labeling/src/main/java/core_labels/WrappingLines.java
@@ -5,7 +5,7 @@ public class WrappingLines extends Duplicates{
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
             for (int j = 0; j < 10; j++) {
-                System.out.println("ololo");
+                System.out.println("pewpew");
             }
         }
     }
